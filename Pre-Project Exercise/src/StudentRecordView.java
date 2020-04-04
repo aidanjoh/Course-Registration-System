ac8f5435f1e1938d6626d14d498abe66dee36dc2
aidanjoh <43867551+aidanjoh@users.noreply.github.com>
2020-04-04 16:54:31 -0600

    First simple version of StudentRecordView

--- Pre-Project Exercise/src/StudentRecordView.java
+++ Pre-Project Exercise/src/StudentRecordView.java
@@ -1,3 +1,5 @@
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 
 /**
@@ -9,11 +11,128 @@ import javax.swing.*;
  */
 public class StudentRecordView extends JFrame
 {
-	private JTextField title = new JTextField();
+	//For the main panel
+	private JLabel title = new JLabel("An Application to Maintain Student Records");
 	
 	private JButton insertButton = new JButton("Insert");
 	private JButton findButton = new JButton("Find");
 	private JButton browseButton = new JButton("Browse");
-	private JButton createTree = new JButton("Create Tree from File");
+	private JButton createTreeButton = new JButton("Create Tree from File");
+	
+	private JTextField studentRecords = new JTextField();
+	
+	//For the input panel
+	private JLabel fileName = new JLabel("Enter the file name:");
+	
+	private JTextField inputtedFile = new JTextField();
+	
+	private JButton cancelButton = new JButton("Cancel");
+	private JButton okButton = new JButton("OK");
+	
+	
+	//For the 
+	public StudentRecordView()
+	{
+		super("Student Records");
+		
+		JPanel studentRecordPanel = new JPanel();
+		
+		setSize(400, 400);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		studentRecordPanel.add(title);
+		studentRecordPanel.add(insertButton);
+		studentRecordPanel.add(findButton);
+		studentRecordPanel.add(browseButton);
+		studentRecordPanel.add(createTreeButton);
+		studentRecordPanel.add(studentRecords);
+		
+		add(studentRecordPanel);
+	}
+	
+	/**
+	 * 
+	 * @param records
+	 */
+	public void setStudentRecords(String records)
+	{
+		studentRecords.setText(records);
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public String getFileName()
+	{
+		return getTextFromTextBox(inputtedFile);
+	}
+	
+	/**
+	 * 
+	 * @param errorMessage
+	 */
+	public void displayErrorMessage(String errorMessage)
+	{
+		JOptionPane.showMessageDialog(this, errorMessage);
+	}
+	
+	/**
+	 * 
+	 * @param textField
+	 * @return
+	 */
+	public String getTextFromTextBox(JTextField textField)
+	{
+		return textField.getText();
+	}
+	
+	/**
+	 * 
+	 * @param textField
+	 * @return
+	 */
+	public int getIntFromTextBox(JTextField textField)
+	{
+		return Integer.parseInt(textField.getText());
+	}
+	
+	//Button Listener Functions
+	
+	/**
+	 * 
+	 * @param listenForInsertButton
+	 */
+	public void addInsertButtonListener(ActionListener listenForInsertButton)
+	{
+		insertButton.addActionListener(listenForInsertButton);
+	}
+	
+	/**
+	 * 
+	 * @param listenForBrowseButton
+	 */
+	public void addBrowseButtonListener(ActionListener listenForBrowseButton)
+	{
+		browseButton.addActionListener(listenForBrowseButton);
+	}
+	
+	/**
+	 * 
+	 * @param listenForFindButton
+	 */
+	public void addFindButtonListener(ActionListener listenForFindButton)
+	{
+		findButton.addActionListener(listenForFindButton);
+	}
+	
+	/**
+	 * 
+	 * @param listenForCreateTreeButton
+	 */
+	public void addCreateTreeButtonListener(ActionListener listenForCreateTreeButton)
+	{
+		createTreeButton.addActionListener(listenForCreateTreeButton);
+	}
 	
 }
