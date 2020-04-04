369cb529c39cad4d56843f7b6d90f6d3a140d67a
aidanjoh <43867551+aidanjoh@users.noreply.github.com>
2020-04-04 15:53:50 -0600

    Created the view file for Student Record MVC

---
+++ Pre-Project Exercise/src/StudentRecordView.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+/**
+ * 
+ * @author Aidan Johnson and Michele Piperni
+ * @version 1.0
+ * @since April 4, 2020
+ *
+ */
+public class StudentRecordView extends JFrame
+{
+
+}
