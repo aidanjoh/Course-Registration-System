5f94cf0b0c9f2f16008193d9ef5a24d8f295d421
aidanjoh <43867551+aidanjoh@users.noreply.github.com>
2020-04-11 14:02:40 -0600

    Got the Student Courses Gui done

--- CourseRegistrationSystemFinalProject/src/clientView/ViewCatalogueCourses.java
+++ CourseRegistrationSystemFinalProject/src/clientView/ViewCatalogueCourses.java
@@ -1,5 +1,14 @@
 package clientView;
 
-public class ViewCatalogueCourses {
+import javax.swing.JDialog;
+
+/**
+ * 
+ * @author Aidan Johnson and Michele Piperni
+ * @version 1.0
+ * @since April 10, 2020
+ */
+public class ViewCatalogueCourses extends JDialog
+{
 
 }
--- CourseRegistrationSystemFinalProject/src/clientView/ViewStudentsCourses.java
+++ CourseRegistrationSystemFinalProject/src/clientView/ViewStudentsCourses.java
@@ -1,5 +1,96 @@
 package clientView;
 
-public class ViewStudentsCourses {
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
+/**
+ * 
+ * @author Aidan Johnson and Michele Piperni
+ * @version 1.0
+ * @since April 10, 2020
+ *
+ */
+public class ViewStudentsCourses extends JDialog
+{
+	/**
+	 * 
+	 */
+	private JLabel title;
+	
+	/**
+	 * 
+	 */
+	private JTextArea listOfStudentsCourses;
+	
+	/**
+	 * 
+	 */
+	private JButton returnButton;
+	
+	/**
+	 * 
+	 */
+	public ViewStudentsCourses(StartUpMenuGUI mainFrame)
+	{
+		super(mainFrame, "Student's Courses", ModalityType.DOCUMENT_MODAL);
+		this.setSize(400, 400);
+		this.setLocationRelativeTo(mainFrame);
+		
+		JPanel titlePanel = new JPanel();
+		JPanel buttonPanel = new JPanel();
+		
+		title = new JLabel("Your Courses");
+		title.setFont(new Font("Helvetica", Font.BOLD, 30));
+		title.setForeground(Color.black);
+		title.setSize(100, 100);
+		title.setLocation(200, 10);
+		titlePanel.add(title);
+		
+		listOfStudentsCourses = new JTextArea();
+		listOfStudentsCourses.setBackground(Color.LIGHT_GRAY);
+		
+		//Making it so the user can not edit the courses displayed
+		listOfStudentsCourses.setEditable(false);
+		
+		JScrollPane studentCourses = new JScrollPane(listOfStudentsCourses);
+		studentCourses.setSize(600, 250);
+		studentCourses.setLocation(0, 50);
+		
+		returnButton = new JButton("Return to Main Menu");
+		returnButton.setSize(200, 50);
+		returnButton.setLocation(185, 300);
+		returnButton.setForeground(Color.black);
+		returnButton.setBackground(Color.LIGHT_GRAY);
+		returnButton.setFont(new Font("Helvetica", Font.BOLD, 20));
+		buttonPanel.add(returnButton);
+		
+		this.add("Center", studentCourses);
+		this.add("South", buttonPanel);
+		this.add("North", titlePanel);
+	}
+	
+	/**
+	 * Adds the action listener for the return to main menu button.
+	 * 
+	 * @param listenForReturnButton ActionListener object for return button.
+	 */
+	public void addReturnButtonListener(ActionListener listenForReturnButton)
+	{
+		returnButton.addActionListener(listenForReturnButton);
+	}
+	
+	public static void main(String[] args)
+	{
+		StartUpMenuGUI mainFrame = new StartUpMenuGUI();
+		ViewStudentsCourses view = new ViewStudentsCourses(mainFrame);
+		mainFrame.setVisible(true);
+		view.setVisible(true);
+	}
 }
